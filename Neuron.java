78a3924f5b280590b5b238d7f9d7e506c9569b88
Soar <Soar@DESKTOP-3PM0IGD>
2019-05-08 21:07:13 +0200

    .

--- Neuron.java
+++ Neuron.java
@@ -2,13 +2,25 @@ import java.util.*;
 
 public class Neuron{
   
-  public double compute(ArrayList input_wheigts, ArrayList inputs){
+  public double compute(ArrayList<Double> input_wheigts, ArrayList<Double> inputs){
     //hier wird logistische Funktion und Summe als Transfer bzw Aktivierungsfunktion genutzt. TODO durch bel. Funktionen ersetzen
-    double sum = 0;
+	/*double sum = 0;
     for(int i = 0; i <inputs.size(); ++i){
       sum += (double) inputs.get(i)* (double) input_wheigts.get(i);
-    }
-    return 1/(1+Math.exp(-sum));
+    }*/
+    
+    
+    //Berechnung der Aktivierungsfunktion RBF
+    double distance = 0; //rh
+    for(int i = 0; i <inputs.size(); ++i){
+        distance += Math.sqrt( Math.pow( (double) inputs.get(i) - (double) input_wheigts.get(i),2) ); //abstandsberechnung
+     }
+    
+    double varianz=2; //bel. aber fest
+    
+    double returnValue = Math.exp(-Math.pow(distance,2)/2*varianz); //f(rh)
+    
+    return returnValue;
   }
   
 }
